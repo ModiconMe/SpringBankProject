ae2077911117a83c11b536547d9654e9fe9eb776
saeed <rebeelsc@gmail.com>
2023-02-07 21:10:52 +0700

    add user command

---
+++ UserModule/user-command-api/src/main/java/com/modicon/user/command/api/commands/RegisterUserCommand.java
@@ -0,0 +1,14 @@
+package com.modicon.user.command.api.commands;
+
+import com.modicon.user.core.models.User;
+import lombok.Builder;
+import lombok.Data;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
+
+@Data
+@Builder
+public class RegisterUserCommand {
+    @TargetAggregateIdentifier
+    private String id;
+    private User user;
+}
