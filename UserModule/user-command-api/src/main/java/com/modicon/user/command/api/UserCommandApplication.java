fa5623148d4f63aa241ff2cd7131819283055459
saeed <rebeelsc@gmail.com>
2023-02-07 20:56:15 +0700

    add axon config

--- UserModule/user-command-api/src/main/java/com/modicon/user/command/api/UserCommandApplication.java
+++ UserModule/user-command-api/src/main/java/com/modicon/user/command/api/UserCommandApplication.java
@@ -1,9 +1,12 @@
 package com.modicon.user.command.api;
 
+import com.modicon.user.core.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserCommandApplication {
 
     public static void main(String[] args) {
--- UserModule/user-query-api/src/main/java/com/modicon/user/query/api/UserQueryApplication.java
+++ UserModule/user-query-api/src/main/java/com/modicon/user/query/api/UserQueryApplication.java
@@ -1,9 +1,12 @@
 package com.modicon.user.query.api;
 
+import com.modicon.user.core.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AxonConfig.class})
 public class UserQueryApplication {
 
     public static void main(String[] args) {
