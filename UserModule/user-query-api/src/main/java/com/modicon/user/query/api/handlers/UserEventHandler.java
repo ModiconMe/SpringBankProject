3f144207ba2c965ebaeffa70b23457cc6c11657f
saeed <rebeelsc@gmail.com>
2023-02-08 22:37:25 +0700

    UserEventHandler and UserQueryHandler refactor

--- UserModule/user-query-api/src/main/java/com/modicon/user/query/api/handlers/UserEventHandler.java
+++ UserModule/user-query-api/src/main/java/com/modicon/user/query/api/handlers/UserEventHandler.java
@@ -3,9 +3,42 @@ package com.modicon.user.query.api.handlers;
 import com.modicon.user.core.events.UserRegisteredEvent;
 import com.modicon.user.core.events.UserRemovedEvent;
 import com.modicon.user.core.events.UserUpdatedEvent;
+import com.modicon.user.query.api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.config.ProcessingGroup;
+import org.axonframework.eventhandling.EventHandler;
+import org.springframework.stereotype.Service;
 
 public interface UserEventHandler {
     void on(UserRegisteredEvent event);
     void on(UserUpdatedEvent event);
     void on(UserRemovedEvent event);
+
+    @Slf4j
+    @RequiredArgsConstructor
+    @Service
+    @ProcessingGroup("user-group")
+    class Base implements UserEventHandler {
+
+        private final UserRepository userRepository;
+
+        @EventHandler
+        @Override
+        public void on(UserRegisteredEvent event) {
+            userRepository.save(event.getUser());
+        }
+
+        @EventHandler
+        @Override
+        public void on(UserUpdatedEvent event) {
+            userRepository.save(event.getUser());
+        }
+
+        @EventHandler
+        @Override
+        public void on(UserRemovedEvent event) {
+            userRepository.deleteById(event.getId());
+        }
+    }
 }
--- UserModule/user-query-api/src/main/java/com/modicon/user/query/api/handlers/UserQueryHandler.java
+++ UserModule/user-query-api/src/main/java/com/modicon/user/query/api/handlers/UserQueryHandler.java
@@ -1,12 +1,49 @@
 package com.modicon.user.query.api.handlers;
 
+import com.modicon.user.core.models.User;
 import com.modicon.user.query.api.dto.UserLookupResponse;
 import com.modicon.user.query.api.queries.FindAllUsersQuery;
 import com.modicon.user.query.api.queries.FindUserByIdQuery;
 import com.modicon.user.query.api.queries.SearchUsersQuery;
+import com.modicon.user.query.api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.axonframework.queryhandling.QueryHandler;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public interface UserQueryHandler {
     UserLookupResponse getUserById(FindUserByIdQuery query);
     UserLookupResponse searchUsers(SearchUsersQuery query);
     UserLookupResponse getAllUsers(FindAllUsersQuery query);
+
+    @RequiredArgsConstructor
+    @Service
+    class Base implements UserQueryHandler {
+
+        private final UserRepository userRepository;
+
+        @QueryHandler
+        @Override
+        public UserLookupResponse getUserById(FindUserByIdQuery query) {
+            List<User> users = new ArrayList<>();
+            users.add(Objects.requireNonNull(userRepository.findById(query.getId()).orElse(null)));
+            return new UserLookupResponse(users);
+        }
+
+        @QueryHandler
+        @Override
+        public UserLookupResponse searchUsers(SearchUsersQuery query) {
+            List<User> usersByFilterRegex = userRepository.findUsersByFilterRegex(query.getFilter());
+            return new UserLookupResponse(usersByFilterRegex);
+        }
+
+        @QueryHandler
+        @Override
+        public UserLookupResponse getAllUsers(FindAllUsersQuery query) {
+            return new UserLookupResponse(userRepository.findAll());
+        }
+    }
 }
