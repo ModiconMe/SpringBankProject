6385c1226c5a3854cd304b01d6db34f784eb2e44
saeed <rebeelsc@gmail.com>
2023-02-09 11:03:00 +0700

    jwt auth

--- UserModule/user-authorization/src/main/java/com/modicon/user/auth/configuration/WebSecurityConfig.java
+++ UserModule/user-authorization/src/main/java/com/modicon/user/auth/configuration/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.modicon.user.auth.configuration;
 
 import com.modicon.user.auth.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
