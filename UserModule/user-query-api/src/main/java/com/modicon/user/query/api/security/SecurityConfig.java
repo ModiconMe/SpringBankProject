cb23396483b45f09a0772f533191d908e126f563
saeed <rebeelsc@gmail.com>
2023-02-09 09:22:10 +0700

    craete bean of PasswordEncoder

--- UserModule/user-query-api/src/main/java/com/modicon/user/query/api/security/SecurityConfig.java
+++ UserModule/user-query-api/src/main/java/com/modicon/user/query/api/security/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
@@ -32,6 +34,11 @@ public class SecurityConfig {
                 .anyRequest().authenticated()
                 .and()
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 
 }
